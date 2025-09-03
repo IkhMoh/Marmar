d66b0a2dd65fc9ea03f5ad93210027f11c790e36
ikhmoh01 <ikhlefmohamed037@gmail.com>
2025-09-03 09:55:04 +0100

    adding logic and ui of content of the post card

--- src/components/client/PostCard.tsx
+++ src/components/client/PostCard.tsx
@@ -3,13 +3,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { Post } from "@/types/post";
+import EmojiPicker from "emoji-picker-react";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Heart, MessageCircle, Send, Volume2, VolumeOff } from "lucide-react";
-import { MenuDialog } from "../server/MenuDialog";
+import { Heart, MessageCircle, Smile, Volume2, VolumeOff } from "lucide-react";
 import { SaveDialog } from "../server/SaveDialog";
+import { SendDialog } from "../server/SendDialog";
 
 import { toggleMute } from "@/store/slice/videoSlice";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import Link from "next/link";
+import { MenuDialog } from "../server/MenuDialog";
 
 interface PostCardProps {
   post: Post;
@@ -19,8 +23,13 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [paused, setPaused] = useState(false);
 
+  const [expanded, setExpanded] = useState(false);
+
   const muted = useAppSelector((state) => state.video.muted);
   const dispatch = useAppDispatch();
+
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [comment, setComment] = useState("");
 
   // mute/unmute
   const handleToggleMute = (e?: React.MouseEvent) => {
@@ -43,7 +52,7 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
 
     // observer: auto play/pause when visible
     const observer = new IntersectionObserver(
-      (entries) => { 
+      (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
             v.play().catch(() => {});
@@ -73,7 +82,7 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
   }, []);
 
   return (
-    <div className="border-b rounded-lg mb-4 dark:bg-gray-800 w-[468px] h-[878px]">
+    <div className="border-b rounded-lg mb-4 dark:bg-gray-800 w-[468px] h-fit pb-4">
       {/* Header */}
       <div className="flex justify-between p-0.5 pb-3">
         <div className="flex items-center ">
@@ -96,7 +105,7 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
 
       {/* Content */}
       {post.image.length > 0 && (
-        <div className="w-full relative h-[600px]">
+        <div className="w-full relative h-fit">
           {post.type === "video" ? (
             <div
               className="relative w-full h-[600px] bg-black overflow-hidden rounded-[3px]"
@@ -148,28 +157,100 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
               )}
             </div>
           ) : (
-            <Image
-              src={
-                post.image[0].startsWith("http")
-                  ? post.image[0]
-                  : `/images/posts/${post.image[0]}`
-              }
-              alt={post.title || "Post image"}
-              fill
-              className="object-cover rounded-md"
-            />
+            <div className="relative w-full h-[468px] bg-black overflow-hidden rounded-[3px]">
+              {" "}
+              <Image
+                src={
+                  post.image[0].startsWith("http")
+                    ? post.image[0]
+                    : `/images/posts/${post.image[0]}`
+                }
+                alt={post.title || "Post image"}
+                fill
+                className="object-cover w-full h-full"
+              />
+            </div>
           )}
         </div>
       )}
 
       {/* Actions */}
-      <div className="flex justify-between pt-2">
-        <div className="flex space-x-4">
-          <Heart size={26} />
-          <MessageCircle size={26} />
-          <Send size={26} />
+      <div className="max-h-[505px]">
+        <div className="flex justify-between my-1 py-2">
+          <div className="flex space-x-4">
+            <Heart size={26} />
+            <Link href={`/post/${post.id}`}>
+              <MessageCircle size={26} />
+            </Link>
+            <SendDialog />
+          </div>
+          <SaveDialog />
         </div>
-        <SaveDialog />
+        <div className="text-sm font-medium">
+          {post.likes ? post.likes : 390} likes
+        </div>
+
+        {/* Body text with expand/collapse */}
+        <div
+          className={`text-sm text-gray-900 dark:text-gray-200 ${
+            expanded
+              ? ""
+              : "overflow-hidden [display:-webkit-box] [-webkit-line-clamp:2] [-webkit-box-orient:vertical]"
+          }`}
+        >
+          {post.body}
+        </div>
+        <div className="w-full h-full flex items-center justify-end">
+          {(post.body?.length ?? 78) > 80 && (
+            <button
+              onClick={() => setExpanded(!expanded)}
+              className="text-gray-600 text-sm font-semibold mt-1 "
+            >
+              {expanded ? "Hide" : "More"}
+            </button>
+          )}
+        </div>
+
+        <Link href={`/post/${post.id}`}>
+          <div className="text-sm text-gray-700 mt-2">
+            View all <span className="font-bold">{post.comments_count}</span>{" "}
+            comments
+          </div>
+        </Link>
+
+        <div className="text-sm text-gray-700 mt-2 flex min-h-6 h-fit relative">
+          <textarea
+            className="min-h-4 h-fit w-full border-none outline-none resize-none overflow-y-hidden leading-none p-0"
+            placeholder="Add a comment…"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+
+          <div className="flex items-center h-fit space-x-2 ">
+            <div className="font-bold text-blue-700"> post</div>
+
+            <div className="relative">
+              <button
+                type="button"
+                onClick={() => setShowEmojiPicker((prev) => !prev)}
+              >
+                <Smile size={16} />
+              </button>
+
+              {/* البوب أب */}
+              {showEmojiPicker && (
+                <div className="absolute bottom-8 right-0 z-50">
+                  <EmojiPicker
+                    onEmojiClick={(emojiData) => {
+                      setComment((prev) => prev + emojiData.emoji);
+                      setShowEmojiPicker(false);
+                    }}
+                  />
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
