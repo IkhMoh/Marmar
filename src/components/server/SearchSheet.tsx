dc8d92e15c0a635861697369c5aa4ef7b172aec4
ikhmoh01 <ikhlefmohamed037@gmail.com>
2025-10-27 15:30:10 +0100

    editing for text in searchSheet

--- src/components/server/SearchSheet.tsx
+++ src/components/server/SearchSheet.tsx
@@ -1,12 +1,8 @@
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
   Sheet,
-  SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
@@ -22,27 +18,21 @@ export function SheetDemo() {
       </SheetTrigger>
       <SheetContent side="left">
         <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when you&apos;re done.
-          </SheetDescription>
+          <SheetTitle className="text-md font-bold pl-3">Search</SheetTitle>
         </SheetHeader>
         <div className="grid flex-1 auto-rows-min gap-6 px-4">
           <div className="grid gap-3">
             <Label htmlFor="sheet-demo-name">Name</Label>
-            <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
-          </div>
-          <div className="grid gap-3">
-            <Label htmlFor="sheet-demo-username">Username</Label>
-            <Input id="sheet-demo-username" defaultValue="@peduarte" />
+            <Input id="sheet-demo-name" defaultValue="" />
           </div>
         </div>
-        <SheetFooter>
-          <Button type="submit">Save changes</Button>
-          <SheetClose asChild>
-            <Button variant="outline">Close</Button>
-          </SheetClose>
-        </SheetFooter>
+        <div className="h-full">
+          <hr className="mt-2" />
+          <h1 className="text-gray-500 p-6 pl-4 font-bold">Resent</h1>
+          <div className="h-full w-full  flex justify-center">
+            <h1>No recent searches.</h1>
+          </div>
+        </div>
       </SheetContent>
     </Sheet>
   );
