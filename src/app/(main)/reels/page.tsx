4c09b0a9d17e00c01ba4ec57c45902bbc06dac8e
ikhmoh01 <ikhlefmohamed037@gmail.com>
2025-11-17 17:46:45 +0100

    adding scrolling reels

--- src/app/(main)/reels/page.tsx
+++ src/app/(main)/reels/page.tsx
@@ -1,18 +1,27 @@
 import Reel from "@/components/client/Reel";
 import React from "react";
 
-const page = () => {
+const Page = () => {
   return (
-    <div className="h-screen w-full flex flex-col justify-center items-center">
-      <Reel />
-      {/*
-      <Reel />
-      <Reel />
-      <Reel />
-      <Reel />
-       */}
+    <div className="h-screen w-full overflow-y-scroll snap-y snap-mandatory pt-4">
+      {/* {reels.map((item) => (
+        <section key={item.id} className="h-screen snap-start">
+          <Reel data={item} />
+        </section>
+      ))} */}
+
+      {/* Example: multiple reels */}
+      <section className="snap-center  h-screen flex items-center justify-center overflow-hidden -mt-[3vh]  ">
+        <Reel />
+      </section>
+      <section className="snap-center  h-screen flex items-center justify-center overflow-hidden -mt-[3vh]  ">
+        <Reel />
+      </section>
+      <section className="snap-center  h-screen flex items-center justify-center overflow-hidden -mt-[3vh]  ">
+        <Reel />
+      </section>
     </div>
   );
 };
 
-export default page;
+export default Page;
