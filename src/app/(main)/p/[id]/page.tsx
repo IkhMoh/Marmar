d89df98b6f8f6ed178fdaea992ccd1adae33a3f5
ikhmoh01 <ikhlefmohamed037@gmail.com>
2025-11-29 11:20:53 +0100

    add " Profile isn`t available" page

---
+++ src/app/(main)/p/[id]/page.tsx
@@ -0,0 +1,7 @@
+const page = () => {
+  return (
+    <div className="bg-red-300 w-screen h-screen flex justify-center items-center mx-40 "></div>
+  );
+};
+
+export default page;
---
+++ src/app/(main)/p/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="h-screen w-screen bg-neutral-100 flex justify-center items-center ">
+      <div className="h-fit w-fit  px-6 py-3 flex flex-col justify-center items-center space-y-4">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="font-bold text-2xl">Profile isn&apos;t available</h1>
+          <p className="font-medium ">
+            The link may be broken, or the profile may have been removed.
+          </p>
+        </div>
+        <Button color="blue">See more in Instagram</Button>
+      </div>
+    </div>
+  );
+};
+
+export default page;
