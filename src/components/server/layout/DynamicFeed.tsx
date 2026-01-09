afd2303caddaffde1e85df4fd5ea0bd2067caca3
ikhmoh01 <ikhlefmohamed037@gmail.com>
2026-01-09 16:06:43 +0100

    adding Skeleton for posts

--- src/components/server/layout/DynamicFeed.tsx
+++ src/components/server/layout/DynamicFeed.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Stories from "../../../features/stories/components/Stories";
 import Feed from "./Feed";
-
+import { PostsSkeleton } from "@/features/posts/components/PostsSkeleton";
 
 const DynamicFeed = () => {
   return (
     <div className="h-full w-[650px] pt-3 ">
       <Stories />
-      <Feed />
+      <Suspense fallback={<PostsSkeleton />}>
+        <Feed />
+      </Suspense>
     </div>
   );
 };
